16d94aeb843f812370828b97ebe1922e922f4207
Diman <pok3be3ga2004@gmail.com>
2024-06-12 14:24:23 +0300

    edits

---
+++ Assets/Scripts/Ground/Ground.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    public GameObject cubePrefab; // Префаб куба
+
+    public int width = 100;
+    public int depth = 100;
+    public float min;
+    public float max;
+    public float spacing = 1.1f;
+
+    public List<GameObject> _objects = new List<GameObject>();
+
+    [ContextMenu ("Create")]
+    public void Create()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < depth; z++)
+            {
+                //float y = CalculatePerlinNoise(x + spacing, z + spacing);
+                //Vector3 pos = new Vector3(x - width / 2, y + spacing, z - depth / 2);
+                Vector3 pos = new Vector3(x * spacing, Random.Range(min, max), z * spacing);
+                //Vector3 pos = new Vector3(x * spacing, Mathf.PerlinNoise(x * spacing, z * spacing), z * spacing);
+                GameObject newCube = Instantiate(cubePrefab, pos, Quaternion.identity);
+                _objects.Add(newCube);
+            }
+        }
+    }
+    float CalculatePerlinNoise(float x, float y)
+    {
+        return Mathf.PerlinNoise(x, y);
+    }
+
+    [ContextMenu("Delete")]
+    public void Delete()
+    {
+        for(int x = 0; x < _objects.Count; x++)
+        {
+            _objects[x].SetActive(false);
+        }
+        _objects.Clear();
+    }
+}
---
+++ Assets/Scripts/Ground/PerlinNoizeTransform.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerlinNoizeTransform : MonoBehaviour
+{
+    //private void OnEnable()
+    //{
+    //    GetTransform();
+    //}
+    public void GetTransform()
+    {
+        float y = Mathf.PerlinNoise(transform.position.x, transform.position.z);
+        transform.position = new Vector3(transform.position.x, y, transform.position.z);
+    }
+    private void Update()
+    {
+        GetTransform();
+    }
+}
