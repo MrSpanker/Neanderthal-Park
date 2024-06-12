78007dd54fd51af417f4161eedac982d572f37f9
Diman <pok3be3ga2004@gmail.com>
2024-06-12 21:02:54 +0300

    Edit level 1

--- Assets/Scripts/Ground/Water.cs
+++ Assets/Scripts/Ground/Water.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class Water : MonoBehaviour
 {
+    [SerializeField] float _down;
     void Update()
     {
         float y = Mathf.PerlinNoise(transform.position.x + Time.time, transform.position.z + Time.time);
 
-        transform.position = new Vector3(transform.position.x, y, transform.position.z);
+        transform.position = new Vector3(transform.position.x, y + _down, transform.position.z);
     }
 }
