b34f5f760188e68b7223ea3dcd2b163b5481731a
kudrinkonstantine <wildmain8295@gmail.com>
2024-06-13 21:38:31 +0300

    Placement, sounds, materials
    placement

--- Assets/Scripts/Placement/PlacementSystem.cs
+++ Assets/Scripts/Placement/PlacementSystem.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 
 public class PlacementSystem : MonoBehaviour
 {
-    [SerializeField] private GameObject _mouseIndicator;
-    [SerializeField] private GameObject _cellIndicator;
     [SerializeField] private PlacementInputManager _placementInputManager;
     [SerializeField] private Grid _grid;
     [SerializeField] private ObjectsDatabaseSO _objectsDatabaseSO;
@@ -14,26 +12,29 @@ public class PlacementSystem : MonoBehaviour
     [SerializeField] private GridData _floorData;
     [SerializeField] private GridData _itemsData;
     [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip _correctPlacement;
+    [SerializeField] private AudioClip _wrongPlacement;
+    [SerializeField] private PreviewSystem _previewSystem;
 
-    private Renderer _previewRenderer;
     private int _selectedObjectIndex = -1;
     private List<GameObject> _placedObjects = new List<GameObject>();
+    private Vector3Int _lastDetectedPosition = Vector3Int.zero;
 
     private void Start()
     {
         StopPlacement();
         _floorData = new GridData();
         _itemsData = new GridData();
-        _previewRenderer = _cellIndicator.GetComponentInChildren<Renderer>();
     }
 
     private void StopPlacement()
     {
         _selectedObjectIndex = -1;
         _gridVisualization.SetActive(false);
-        _cellIndicator.SetActive(false);
+        _previewSystem.StopShowingPreview();
         _placementInputManager.OnClicked -= PlaceStructure;
         _placementInputManager.OnExit -= StopPlacement;
+        _lastDetectedPosition = Vector3Int.zero;
     }
 
     public void StartPlacement(int ID)
@@ -41,26 +42,24 @@ public class PlacementSystem : MonoBehaviour
         StopPlacement();
         _selectedObjectIndex = _objectsDatabaseSO.ObjectsData.FindIndex(data => data.ID == ID);
 
-        if(_selectedObjectIndex <0)
+        if (_selectedObjectIndex < 0)
         {
             Debug.LogError($"No ID found {ID}");
             return;
         }
 
         _gridVisualization.SetActive(true);
-        _cellIndicator.SetActive(true);
+        _previewSystem.StartShowingPlacementPreview(_objectsDatabaseSO.ObjectsData[_selectedObjectIndex].Prefab, _objectsDatabaseSO.ObjectsData[_selectedObjectIndex].Size);
         _placementInputManager.OnClicked += PlaceStructure;
         _placementInputManager.OnExit += StopPlacement;
     }
 
     private void PlaceStructure()
     {
-        if(_placementInputManager.IsPointerOverUI())
+        if (_placementInputManager.IsPointerOverUI())
         {
             return;
         }
-
-
 
         Vector3 mousePosition = _placementInputManager.GetSelectedMapPosition();
         Vector3Int gridPosition = _grid.WorldToCell(mousePosition);
@@ -68,14 +67,20 @@ public class PlacementSystem : MonoBehaviour
         bool placementValidity = CheckPlacementValidity(gridPosition, _selectedObjectIndex);
 
         if (placementValidity == false)
+        {
+            _audioSource.PlayOneShot(_wrongPlacement);
             return;
+        }
 
-        _audioSource.Play();
+        _audioSource.PlayOneShot(_correctPlacement);
         GameObject newObject = Instantiate(_objectsDatabaseSO.ObjectsData[_selectedObjectIndex].Prefab);
         newObject.transform.position = _grid.CellToWorld(gridPosition);
-        _placedObjects.Add( newObject );
+        newObject.transform.position = new Vector3(newObject.transform.position.x, mousePosition.y, newObject.transform.position.z);  // redacted
+        _placedObjects.Add(newObject);
         GridData selectedData = _objectsDatabaseSO.ObjectsData[_selectedObjectIndex].ID == 0 ? _floorData : _itemsData;
         selectedData.AddObjectAt(gridPosition, _objectsDatabaseSO.ObjectsData[_selectedObjectIndex].Size, _objectsDatabaseSO.ObjectsData[_selectedObjectIndex].ID, _placedObjects.Count - 1);
+
+        _previewSystem.UpdatePosition(_grid.CellToWorld(gridPosition), false);
     }
 
     private bool CheckPlacementValidity(Vector3Int gridPosition, int selectedObjectIndex)
@@ -94,11 +99,14 @@ public class PlacementSystem : MonoBehaviour
 
         Vector3 mousePosition = _placementInputManager.GetSelectedMapPosition();
         Vector3Int gridPosition = _grid.WorldToCell(mousePosition);
-        bool placementValidity = CheckPlacementValidity(gridPosition, _selectedObjectIndex);
 
-        _previewRenderer.material.color = placementValidity ? Color.white : Color.red;
+        if (_lastDetectedPosition != gridPosition)
+        {
+            bool placementValidity = CheckPlacementValidity(gridPosition, _selectedObjectIndex);
 
-        _mouseIndicator.transform.position = mousePosition;
-        _cellIndicator.transform.position = _grid.CellToWorld(gridPosition);
+            //_mouseIndicator.transform.position = mousePosition;
+            _previewSystem.UpdatePosition(_grid.CellToWorld(gridPosition), placementValidity);
+            _lastDetectedPosition = gridPosition;
+        }
     }
 }
--- Assets/Scripts/Placement/PreviewSystem.cs
+++ Assets/Scripts/Placement/PreviewSystem.cs
@@ -1,20 +1,91 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PreviewSystem : MonoBehaviour
 {
+    [SerializeField] private PlacementInputManager _placementInputManager;
     [SerializeField] private float _previewOffset = 0.06f;
     [SerializeField] private GameObject _cellIndicator;
     [SerializeField] private Material _previewMaterialPrefab;
+    [SerializeField] private Renderer _cellIndicatorRenderer;
 
     private GameObject _previewObject;
     private Material _previewMaterialInstance;
 
+    private void Start()
+    {
+        _previewMaterialInstance = new Material(_previewMaterialPrefab);
+        _cellIndicator.SetActive(false);
+    }
+
+    public void StartShowingPlacementPreview(GameObject prefab, Vector2Int size)
+    {
+        _previewObject = Instantiate(prefab);
+        PreparePreview(_previewObject);
+        PrepareIndicator(size);
+        _cellIndicator.SetActive(true);
+    }
+
+    private void PrepareIndicator(Vector2Int size)
+    {
+        if (size.x > 0 || size.y > 0)
+        {
+            _cellIndicator.transform.localScale = new Vector3(size.x, 0.1f, size.y);
+
+            _cellIndicatorRenderer.material.mainTextureScale = size;
+        }
+    }
+
+    private void PreparePreview(GameObject previewObject)
+    {
+        Renderer[] renderers = _previewObject.GetComponentsInChildren<Renderer>();
+
+        foreach (Renderer renderer in renderers)
+        {
+            Material[] materials = renderer.materials;
+
+            for (int i = 0; i < materials.Length; i++)
+            {
+                materials[i] = _previewMaterialInstance;
+            }
+
+            renderer.materials = materials;
+        }
+    }
+
+    public void StopShowingPreview()
+    {
+        _cellIndicator.SetActive(false);
+        Destroy(_previewObject);
+    }
+
+    public void UpdatePosition(Vector3 position, bool validity)
+    {
+        MovePreview(position);
+        MoveCursor(position);
+        ApplyFeedback(validity);
 
 
+    }
 
+    private void ApplyFeedback(bool validity)
+    {
+        Color color = validity ? Color.white : Color.red;
+        color.a = 0.5f;
+        _cellIndicatorRenderer.material.color = color;
+        _previewMaterialInstance.color = color;
+    }
 
+    private void MoveCursor(Vector3 position)
+    {
+        _cellIndicator.transform.position = new Vector3( position.x, _placementInputManager.GetSelectedMapPosition().y, position.z);
 
+    }
 
+    private void MovePreview(Vector3 position)
+    {
+        _previewObject.transform.position = new Vector3(position.x, _placementInputManager.GetSelectedMapPosition().y + _previewOffset, position.z);
+    }
 }
