99d47b36e481d454015bb225e6f50f050efd2173
Diman <pok3be3ga2004@gmail.com>
2024-06-16 15:56:28 +0300

    Update MoodDodik.cs

--- Assets/Scripts/Dodik/MoodDodik.cs
+++ Assets/Scripts/Dodik/MoodDodik.cs
@@ -5,7 +5,11 @@ public class MoodDodik : MonoBehaviour
 {
     [SerializeField] private Slider _moodSlider;
     [SerializeField] private float _speed = 0.1f;
-    [SerializeField] Image _fill;
+    [SerializeField] Image _fillImage;
+    [SerializeField] Image _backgroundImage;
+    [SerializeField] Sprite[] _moodlesSprites;
+    [SerializeField] Color[] _moodlesColors;
+
     private bool _stopMood;
     private void Update()
     {
@@ -13,11 +17,20 @@ public class MoodDodik : MonoBehaviour
             _moodSlider.value -= Time.deltaTime * _speed;
 
         if(_moodSlider.value > 0.7f)
-            _fill.color = Color.green;
+        {
+            _fillImage.color = _moodlesColors[0];
+            _backgroundImage.sprite = _moodlesSprites[0];
+        }
         else if(_moodSlider.value < 0.7f && _moodSlider.value > 0.4f)
-            _fill.color = Color.yellow;
+        {
+            _fillImage.color = _moodlesColors[1];
+            _backgroundImage.sprite = _moodlesSprites[1];
+        }
         else if(_moodSlider.value < 0.4f)
-            _fill.color = Color.red;
+        {
+            _fillImage.color = _moodlesColors[2];
+            _backgroundImage.sprite = _moodlesSprites[2];
+        }
     }
     public void StopMood()
     {
