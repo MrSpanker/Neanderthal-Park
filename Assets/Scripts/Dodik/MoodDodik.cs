88d0ac883279c40b556b635015870c2ee7103fe1
Diman <pok3be3ga2004@gmail.com>
2024-06-16 16:42:54 +0300

    Prefabs edits

--- Assets/Scripts/Dodik/MoodDodik.cs
+++ Assets/Scripts/Dodik/MoodDodik.cs
@@ -5,19 +5,32 @@ public class MoodDodik : MonoBehaviour
 {
     [SerializeField] private Slider _moodSlider;
     [SerializeField] private float _speed = 0.1f;
-    [SerializeField] Image _fill;
+    [SerializeField] Image _fillImage;
+    [SerializeField] Image _backgroundImage;
+    [SerializeField] Sprite[] _moodlesSprites;
+    [SerializeField] Color[] _moodlesColors;
+
     private bool _stopMood;
     private void Update()
     {
-        if(_stopMood == false)
+        if (_stopMood == false)
             _moodSlider.value -= Time.deltaTime * _speed;
 
-        if(_moodSlider.value > 0.7f)
-            _fill.color = Color.green;
-        else if(_moodSlider.value < 0.7f && _moodSlider.value > 0.4f)
-            _fill.color = Color.yellow;
-        else if(_moodSlider.value < 0.4f)
-            _fill.color = Color.red;
+        if (_moodSlider.value > 0.7f)
+        {
+            _fillImage.color = _moodlesColors[0];
+            _backgroundImage.sprite = _moodlesSprites[0];
+        }
+        else if (_moodSlider.value < 0.7f && _moodSlider.value > 0.4f)
+        {
+            _fillImage.color = _moodlesColors[1];
+            _backgroundImage.sprite = _moodlesSprites[1];
+        }
+        else if (_moodSlider.value < 0.4f)
+        {
+            _fillImage.color = _moodlesColors[2];
+            _backgroundImage.sprite = _moodlesSprites[2];
+        }
     }
     public void StopMood()
     {
