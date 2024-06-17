1206d02e7f15b3058b80c025e3c30f2761c54e1e
Diman <pok3be3ga2004@gmail.com>
2024-06-17 19:25:58 +0300

    +

--- Assets/Scripts/Dodik/DodikPerceptionZones.cs
+++ Assets/Scripts/Dodik/DodikPerceptionZones.cs
@@ -1,12 +1,16 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class DodikPerceptionZones : BasePerceptionZones
 {
     public event UnityAction<GameObject> ObjectForSearchDetected;
-
+    [SerializeField] private Collider[] _colliders;
     private ObjectType _currentObjectForSearch;
-
+    private void Start()
+    {
+        StartCoroutine(CheckCollision());
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.TryGetComponent<InterestingPlace>(out _))
@@ -32,8 +36,6 @@ public class DodikPerceptionZones : BasePerceptionZones
             Debug.Log("Обнаружен искомый объект: " + other.gameObject.name);
             ObjectForSearchDetected?.Invoke(other.gameObject);
         }
-
-        
     }
     public void SetSearchObjectTag(ObjectType objectType)
     {
@@ -44,5 +46,18 @@ public class DodikPerceptionZones : BasePerceptionZones
     public ObjectType GetSearchObjectTag()
     {
         return _currentObjectForSearch;
+    }
+    IEnumerator CheckCollision()
+    {
+        yield return new WaitForSeconds(3);
+        for (int i = 0; i < _colliders.Length; i++)
+        {
+            _colliders[i].enabled = false;
+        }
+        for (int i = 0; i < _colliders.Length; i++)
+        {
+            _colliders[i].enabled = true;
+        }
+        StartCoroutine(CheckCollision());
     }
 }
